027dea906a04dc2c5d26e91d57e0d14be90b202a
HippolyteGuigon <guigonhippolyte@yahoo.fr>
2025-12-23 14:30:54 +0100

    added multiprocessing benchmark to bash script

--- benchmark.sh
+++ benchmark.sh
@@ -4,8 +4,14 @@ echo "" && \
 echo "Rust timing Hello" &&\
 time ./rust/hello/target/release/hello && \
 echo "" && \
-echo "Python timing Hello" && \
+echo "Python timing compute" && \
 time python python/compute.py && \
 echo "" && \
-echo "Rust timing Hello" && \
-time ./rust/hello/target/release/compute 
+echo "Rust timing compute" && \
+time ./rust/hello/target/release/compute && \
+echo "" && \
+echo "Python timing compute parrallel multiprocessing" && \
+time python python/compute_parrallel.py && \
+echo "" && \
+echo "Rust timing compute parrallel multiprocessing" && \
+time ./rust/hello/target/release/compute_parrallel 
