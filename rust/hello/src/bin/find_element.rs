0628ebd2eddf2a517f993e4deb2f8a5770fdc278
Hippolyte Guigon <guigonhippolyte@yahoo.fr>
2026-08-22 17:55:49 +0200

    added finding index function

---
+++ rust/hello/src/bin/find_element.rs
@@ -0,0 +1,22 @@
+fn trouver_index(v: &Vec<i32>, cible: i32) -> Option<usize> {
+    for (i, &valeur) in v.iter().enumerate() {
+        if valeur == cible {
+            return Some(i);
+        }
+    }
+    None
+}
+
+fn main() {
+    let nombres = vec![10, 25, 3, 47];
+
+    match trouver_index(&nombres, 3){
+        Some(index)=>println!("L'élement 3 se trouver à l'index : {}", index),
+        None=>println!("L'élement 3 n'est pas dans le vecteur")
+    }
+
+    match trouver_index(&nombres, 100){
+        Some(index)=>println!("L'élement 100 se trouver à l'index : {}", index),
+        None=>println!("L'élement 100 n'est pas dans le vecteur")
+    }
+}
