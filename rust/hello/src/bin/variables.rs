93823033e7bfc07127565eebed4cae51d217f493
Hippolyte Guigon <guigonhippolyte@yahoo.fr>
2026-08-22 14:33:24 +0200

    added if else condition function

--- rust/hello/src/bin/variables.rs
+++ rust/hello/src/bin/variables.rs
@@ -2,4 +2,15 @@ fn main() {
     println!("Bonjour");
     let age = 30;
     println!("J'ai {} ans", age);
+    let is_even = is_even(age);
+    println!("Est-ce que mon âge est pair ? {}", is_even);
+}
+
+fn is_even(num: i32) -> bool {
+    if num % 2 == 0 {
+        true
+    }
+    else {
+        false
+    }
 }
